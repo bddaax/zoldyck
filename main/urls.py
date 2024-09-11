7f5f4c7a719dca11451462c09c5a599c5ee898f8
bddaax <brenda147052gmail.com>
2024-09-11 09:34:50 +0700

    Update main.html

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import index, home
 
-app_name = 'main'  
+app_name = 'main'
 
 urlpatterns = [
-    path('', index, name='index'),  # URL untuk halaman depan
-    path('home/', home, name='home'),  # URL untuk halaman utama
+    path('', index, name='index'),
+    path('home/', home, name='home'),
 ]
