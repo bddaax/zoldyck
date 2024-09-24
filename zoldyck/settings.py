4c1e74372a57bba578e51bad8b58265db954370f
bddaax <brenda147052gmail.com>
2024-09-24 22:17:25 +0700

    Add deploy.yml

--- zoldyck/settings.py
+++ zoldyck/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://brenda-po-zoldyck.pbp.cs.ui.ac.id/", "https://brenda-po-zoldyck.pbp.cs.ui.ac.id/"]
